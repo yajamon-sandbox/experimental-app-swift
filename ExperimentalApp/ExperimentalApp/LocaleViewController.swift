07c8188b1bdbd99248d1e79f2f14438feb24436f
yajamon <yajamon.tatsuki@gmail.com>
2020-08-29 21:55:43 +0900

    LocaleViewControllerを追加

---
+++ ExperimentalApp/ExperimentalApp/LocaleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LocaleViewController.swift
+//  ExperimentalApp
+//
+//  Created by Yajamon on 2020/08/29.
+//
+
+import UIKit
+
+class LocaleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
