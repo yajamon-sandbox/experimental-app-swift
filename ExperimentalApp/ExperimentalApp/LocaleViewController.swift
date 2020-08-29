b2ff4a99ac12f2b1c300fe6aba8025aedb671e1e
yajamon <yajamon.tatsuki@gmail.com>
2020-08-29 22:44:00 +0900

    LanguageCodeを出力する
    Locale.languageCode は NSLocale.languageCode と異なり、
    iOS 10 未満にも対応している。

--- ExperimentalApp/ExperimentalApp/LocaleViewController.swift
+++ ExperimentalApp/ExperimentalApp/LocaleViewController.swift
@@ -9,8 +9,17 @@ import UIKit
 
 class LocaleViewController: UIViewController {
 
+    @IBOutlet weak var textview: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        textview.text = localeInformation()
+    }
+
+    func localeInformation() -> String {
+        let languageCode = Locale.current.languageCode ?? ""
+
+        return "Language Code: \(languageCode)"
     }
 }
